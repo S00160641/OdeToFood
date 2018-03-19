e33b6fd2cfcbb7005aacb03d36e06ec1cf7b4de7
Sophia Price <sophiaprice10@hotmail.com>
2018-03-19 01:24:15 +0000

    enable-migrations, db added

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,24 +1,48 @@
 ﻿using OdeToFood.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        OdeToFoodDb _db = new OdeToFoodDb();
+        public ActionResult Index(string searchTerm = null)
         {
-            var controller = RouteData.Values["controller"];
+            /* var controller = RouteData.Values["controller"];
             var action = RouteData.Values["action"];
             var id = RouteData.Values["id"];
             var message = String.Format("{0}::{1} {2}", controller, action, id);
 
-            ViewBag.Message = message;
+            ViewBag.Message = message; */
 
-            return View();
+            //var model =
+            //    from r in _db.Restaurants
+            //    orderby r.Reviews.Average(review => review.Rating) descending
+            //    select new RestaurantListViewModel
+            //    {
+            //        Id = r.Id,
+            //        Name = r.Name,
+            //        City = r.City,
+            //        Country = r.Country,
+            //        CountOfReviews = r.Reviews.Count()
+            //    };
+
+            var model =
+                _db.Restaurants
+                .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+                .Where( r => searchTerm == null || r.Name.StartsWith(searchTerm))
+                .Take(10) // Can only invoke using this method (takes first 10)
+                .Select(r => new RestaurantListViewModel
+                        {
+                            Id = r.Id,
+                            Name = r.Name,
+                            City = r.City,
+                            Country = r.Country,
+                            CountOfReviews = r.Reviews.Count()
+                        });
+
+            return View(model);
         }
 
         public ActionResult About()
@@ -38,6 +62,16 @@ namespace OdeToFood.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+
+            base.Dispose(disposing);
         }
     }
 }
---
+++ OdeToFood/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace OdeToFood.Migrations
+{
+    using Models;
+    using System.Collections.Generic;
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OdeToFood.Models.OdeToFoodDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "OdeToFood.Models.OdeToFoodDb";
+        }
+
+        protected override void Seed(OdeToFood.Models.OdeToFoodDb context)
+        {
+            context.Restaurants.AddOrUpdate(r => r.Name, // r.Name -> looking to see if restaurant name exists, if yes -> update, else -> add
+                new Restaurant { Name = "Sabatino's", City = "Baltimore", Country = "USA" },
+                new Restaurant { Name = "Great Lake", City = "Chicago", Country = "USA" },
+                new Restaurant
+                {
+                    Name = "Smaka",
+                    City = "Gothenburg",
+                    Country = "Sweden",
+                    Reviews = 
+                    new List<RestaurantReview>
+                    {
+                        new RestaurantReview { Rating = 9, Body = "Great food!", ReviewerName = "Scott" }
+                    }
+                });
+        }
+    }
+}
